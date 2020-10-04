c297d4fd0489d9be309ccc3dec58097c631349ab
Jon Degenhardt <jondegenhardt@users.noreply.github.com>
2020-10-04 16:19:27 -0700

    Add additional format cases.

--- src/helloworld.d
+++ src/helloworld.d
@@ -15,4 +15,10 @@ void main(string[] args)
     writeln("writeln(format(\"%.1f\", 1.06)) => ", format("%.1f", 1.06));
     writeln("writeln(format(\"%.*f\", 1, 1.06)) => ", format("%.*f", 1, 1.06));
 
+    writeln("writeln(format(\"%.0f\", 0.6)) => ", format("%.0f", 0.6));
+    writeln("writeln(format(\"%.*f\", 0, 0.6)) => ", format("%.*f", 0, 0.6));
+
+    writeln("writeln(format(\"%.0f\", 1.6)) => ", format("%.0f", 1.6));
+    writeln("writeln(format(\"%.*f\", 0, 1.6)) => ", format("%.*f", 0, 1.6));
+
 }
