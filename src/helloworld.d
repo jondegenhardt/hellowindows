17b212c8dca46c21c20a22b2b40b145f0f89fe4b
Jon Degenhardt <jondegenhardt@users.noreply.github.com>
2020-10-25 14:46:37 -0700

    More format cases.

--- src/helloworld.d
+++ src/helloworld.d
@@ -31,86 +31,90 @@ void main(string[] args)
     writeln("------");
     writeln();
 
-    /* Some float format tests. */
-    import std.format : format;
+    {
+        /* Some float format tests. */
+        import std.format : format;
 
-    writeln("Float format tests");
+        writeln("Float format tests");
 
-    writeln("writeln(format(\"%.2f\", 12.678)) => ", format("%.2f", 12.678));
-    writeln("writeln(format(\"%.*f\", 12.678)) => ", format("%.*f", 2, 12.678));
+        writeln("writeln(format(\"%.2f\", 12.678)) => ", format("%.2f", 12.678));
+        writeln("writeln(format(\"%.*f\", 12.678)) => ", format("%.*f", 2, 12.678));
 
-    writeln("writeln(format(\"%.1f\", 0.06)) => ", format("%.1f", 0.06));
-    writeln("writeln(format(\"%.*f\", 1, 0.06)) => ", format("%.*f", 1, 0.06));
+        writeln("writeln(format(\"%.1f\", 0.06)) => ", format("%.1f", 0.06));
+        writeln("writeln(format(\"%.*f\", 1, 0.06)) => ", format("%.*f", 1, 0.06));
 
-    writeln("writeln(format(\"%.1f\", 1.06)) => ", format("%.1f", 1.06));
-    writeln("writeln(format(\"%.*f\", 1, 1.06)) => ", format("%.*f", 1, 1.06));
+        writeln("writeln(format(\"%.1f\", 1.06)) => ", format("%.1f", 1.06));
+        writeln("writeln(format(\"%.*f\", 1, 1.06)) => ", format("%.*f", 1, 1.06));
 
-    writeln("writeln(format(\"%.0f\", 0.6)) => ", format("%.0f", 0.6));
-    writeln("writeln(format(\"%.*f\", 0, 0.6)) => ", format("%.*f", 0, 0.6));
+        writeln("writeln(format(\"%.0f\", 0.6)) => ", format("%.0f", 0.6));
+        writeln("writeln(format(\"%.*f\", 0, 0.6)) => ", format("%.*f", 0, 0.6));
 
-    writeln("writeln(format(\"%.0f\", 1.6)) => ", format("%.0f", 1.6));
-    writeln("writeln(format(\"%.*f\", 0, 1.6)) => ", format("%.*f", 0, 1.6));
+        writeln("writeln(format(\"%.0f\", 1.6)) => ", format("%.0f", 1.6));
+        writeln("writeln(format(\"%.*f\", 0, 1.6)) => ", format("%.*f", 0, 1.6));
 
-    writeln("---------------");
+        writeln("---------------");
 
-    writeln("format(\"%.*f\", 0, 0.6) => ", format("%.*f", 0, 0.6));
-    writeln("format(\"%.*f\", 1, 0.6) => ", format("%.*f", 1, 0.6));
-    writeln("format(\"%.*f\", 0, 0.06) => ", format("%.*f", 0, 0.06));
-    writeln("format(\"%.*f\", 1, 0.06) => ", format("%.*f", 1, 0.06));
-    writeln("format(\"%.*f\", 2, 0.06) => ", format("%.*f", 2, 0.06));
-    writeln("format(\"%.*f\", 3, 0.06) => ", format("%.*f", 3, 0.06));
-    writeln("format(\"%.*f\", 0, 9.49999) => ", format("%.*f", 0, 9.49999));
-    writeln("format(\"%.*f\", 1, 9.49999) => ", format("%.*f", 1, 9.49999));
-    writeln("format(\"%.*f\", 0, 9.6) => ", format("%.*f", 0, 9.6));
-    writeln("format(\"%.*f\", 1, 9.6) => ", format("%.*f", 1, 9.6));
-    writeln("format(\"%.*f\", 0, 99.99) => ", format("%.*f", 0, 99.99));
-    writeln("format(\"%.*f\", 1, 99.99) => ", format("%.*f", 1, 99.99));
-    writeln("format(\"%.*f\", 2, 99.99) => ", format("%.*f", 2, 99.99));
-    writeln("format(\"%.*f\", 3, 9999.9996) => ", format("%.*f", 3, 9999.9996));
-    writeln("format(\"%.*f\", 4, 9999.9996) => ", format("%.*f", 4, 9999.9996));
-    writeln("format(\"%.*f\", 4, 99999.99996) => ", format("%.*f", 4, 99999.99996));
-    writeln("format(\"%.*f\", 5, 99999.99996) => ", format("%.*f", 5, 99999.99996));
-    writeln("format(\"%.*f\", 5, 999999.999996) => ", format("%.*f", 5, 999999.999996));
-    writeln("format(\"%.*f\", 6, 999999.999996) => ", format("%.*f", 6, 999999.999996));
+        writeln("format(\"%.*f\", 0, 0.6) => ", format("%.*f", 0, 0.6));
+        writeln("format(\"%.*f\", 1, 0.6) => ", format("%.*f", 1, 0.6));
+        writeln("format(\"%.*f\", 0, 0.06) => ", format("%.*f", 0, 0.06));
+        writeln("format(\"%.*f\", 1, 0.06) => ", format("%.*f", 1, 0.06));
+        writeln("format(\"%.*f\", 2, 0.06) => ", format("%.*f", 2, 0.06));
+        writeln("format(\"%.*f\", 3, 0.06) => ", format("%.*f", 3, 0.06));
+        writeln("format(\"%.*f\", 0, 9.49999) => ", format("%.*f", 0, 9.49999));
+        writeln("format(\"%.*f\", 1, 9.49999) => ", format("%.*f", 1, 9.49999));
+        writeln("format(\"%.*f\", 0, 9.6) => ", format("%.*f", 0, 9.6));
+        writeln("format(\"%.*f\", 1, 9.6) => ", format("%.*f", 1, 9.6));
+        writeln("format(\"%.*f\", 0, 99.99) => ", format("%.*f", 0, 99.99));
+        writeln("format(\"%.*f\", 1, 99.99) => ", format("%.*f", 1, 99.99));
+        writeln("format(\"%.*f\", 2, 99.99) => ", format("%.*f", 2, 99.99));
+        writeln("format(\"%.*f\", 3, 9999.9996) => ", format("%.*f", 3, 9999.9996));
+        writeln("format(\"%.*f\", 4, 9999.9996) => ", format("%.*f", 4, 9999.9996));
+        writeln("format(\"%.*f\", 4, 99999.99996) => ", format("%.*f", 4, 99999.99996));
+        writeln("format(\"%.*f\", 5, 99999.99996) => ", format("%.*f", 5, 99999.99996));
+        writeln("format(\"%.*f\", 5, 999999.999996) => ", format("%.*f", 5, 999999.999996));
+        writeln("format(\"%.*f\", 6, 999999.999996) => ", format("%.*f", 6, 999999.999996));
 
-    writeln("---------------");
+        writeln("---------------");
 
-    writeln("format(\"%.*f\", 0, -0.6) => ", format("%.*f", 0, -0.6));
-    writeln("format(\"%.*f\", 1, -0.6) => ", format("%.*f", 1, -0.6));
-    writeln("format(\"%.*f\", 0, -0.06) => ", format("%.*f", 0, -0.06));
-    writeln("format(\"%.*f\", 1, -0.06) => ", format("%.*f", 1, -0.06));
-    writeln("format(\"%.*f\", 2, -0.06) => ", format("%.*f", 2, -0.06));
-    writeln("format(\"%.*f\", 3, -0.06) => ", format("%.*f", 3, -0.06));
-    writeln("format(\"%.*f\", 0, -9.49999) => ", format("%.*f", 0, -9.49999));
-    writeln("format(\"%.*f\", 1, -9.49999) => ", format("%.*f", 1, -9.49999));
-    writeln("format(\"%.*f\", 0, -9.6) => ", format("%.*f", 0, -9.6));
-    writeln("format(\"%.*f\", 1, -9.6) => ", format("%.*f", 1, -9.6));
-    writeln("format(\"%.*f\", 0, -99.99) => ", format("%.*f", 0, -99.99));
-    writeln("format(\"%.*f\", 1, -99.99) => ", format("%.*f", 1, -99.99));
-    writeln("format(\"%.*f\", 2, -99.99) => ", format("%.*f", 2, -99.99));
-    writeln("format(\"%.*f\", 3, -9999.9996) => ", format("%.*f", 3, -9999.9996));
-    writeln("format(\"%.*f\", 4, -9999.9996) => ", format("%.*f", 4, -9999.9996));
-    writeln("format(\"%.*f\", 4, -99999.99996) => ", format("%.*f", 4, -99999.99996));
-    writeln("format(\"%.*f\", 5, -99999.99996) => ", format("%.*f", 5, -99999.99996));
-    writeln("format(\"%.*f\", 5, -999999.999996) => ", format("%.*f", 5, -999999.999996));
-    writeln("format(\"%.*f\", 6, -999999.999996) => ", format("%.*f", 6, -999999.999996));
+        writeln("format(\"%.*f\", 0, -0.6) => ", format("%.*f", 0, -0.6));
+        writeln("format(\"%.*f\", 1, -0.6) => ", format("%.*f", 1, -0.6));
+        writeln("format(\"%.*f\", 0, -0.06) => ", format("%.*f", 0, -0.06));
+        writeln("format(\"%.*f\", 1, -0.06) => ", format("%.*f", 1, -0.06));
+        writeln("format(\"%.*f\", 2, -0.06) => ", format("%.*f", 2, -0.06));
+        writeln("format(\"%.*f\", 3, -0.06) => ", format("%.*f", 3, -0.06));
+        writeln("format(\"%.*f\", 0, -9.49999) => ", format("%.*f", 0, -9.49999));
+        writeln("format(\"%.*f\", 1, -9.49999) => ", format("%.*f", 1, -9.49999));
+        writeln("format(\"%.*f\", 0, -9.6) => ", format("%.*f", 0, -9.6));
+        writeln("format(\"%.*f\", 1, -9.6) => ", format("%.*f", 1, -9.6));
+        writeln("format(\"%.*f\", 0, -99.99) => ", format("%.*f", 0, -99.99));
+        writeln("format(\"%.*f\", 1, -99.99) => ", format("%.*f", 1, -99.99));
+        writeln("format(\"%.*f\", 2, -99.99) => ", format("%.*f", 2, -99.99));
+        writeln("format(\"%.*f\", 3, -9999.9996) => ", format("%.*f", 3, -9999.9996));
+        writeln("format(\"%.*f\", 4, -9999.9996) => ", format("%.*f", 4, -9999.9996));
+        writeln("format(\"%.*f\", 4, -99999.99996) => ", format("%.*f", 4, -99999.99996));
+        writeln("format(\"%.*f\", 5, -99999.99996) => ", format("%.*f", 5, -99999.99996));
+        writeln("format(\"%.*f\", 5, -999999.999996) => ", format("%.*f", 5, -999999.999996));
+        writeln("format(\"%.*f\", 6, -999999.999996) => ", format("%.*f", 6, -999999.999996));
 
+        writeln("---------------");
 
-    writeln("---------------");
+        writeln("format(\"%.*f\", 0, 0.1) => ", format("%.*f", 0, 0.1));
+        writeln("format(\"%.*f\", 0, 0.4) => ", format("%.*f", 0, 0.4));
+        writeln("format(\"%.*f\", 0, 0.5) => ", format("%.*f", 0, 0.5));
+        writeln("format(\"%.*f\", 0, 0.50001) => ", format("%.*f", 0, 0.50001));
+        writeln("format(\"%.*f\", 0, 0.6) => ", format("%.*f", 0, 0.6));
+        writeln("format(\"%.*f\", 0, 0.60001) => ", format("%.*f", 0, 0.60001));
+        writeln("format(\"%.*f\", 0, 0.7) => ", format("%.*f", 0, 0.7));
+        writeln("format(\"%.*f\", 0, 0.8) => ", format("%.*f", 0, 0.8));
+        writeln("format(\"%.*f\", 0, 0.9) => ", format("%.*f", 0, 0.9));
+        writeln("format(\"%.*f\", 0, 0.99999) => ", format("%.*f", 0, 0.99999));
+        writeln("format(\"%.*f\", 0, 1.0) => ", format("%.*f", 0, 1.0));
 
-    writeln("format(\"%.*f\", 0, 0.1) => ", format("%.*f", 0, 0.1));
-    writeln("format(\"%.*f\", 0, 0.4) => ", format("%.*f", 0, 0.4));
-    writeln("format(\"%.*f\", 0, 0.5) => ", format("%.*f", 0, 0.5));
-    writeln("format(\"%.*f\", 0, 0.50001) => ", format("%.*f", 0, 0.50001));
-    writeln("format(\"%.*f\", 0, 0.6) => ", format("%.*f", 0, 0.6));
-    writeln("format(\"%.*f\", 0, 0.60001) => ", format("%.*f", 0, 0.60001));
-    writeln("format(\"%.*f\", 0, 0.7) => ", format("%.*f", 0, 0.7));
-    writeln("format(\"%.*f\", 0, 0.8) => ", format("%.*f", 0, 0.8));
-    writeln("format(\"%.*f\", 0, 0.9) => ", format("%.*f", 0, 0.9));
-    writeln("format(\"%.*f\", 0, 0.99999) => ", format("%.*f", 0, 0.99999));
-    writeln("format(\"%.*f\", 0, 1.0) => ", format("%.*f", 0, 1.0));
+        double zeroPointSix = 0.6;
+        writeln("format(\"%.*f\", 0, zeroPointSix) => ", format("%.*f", 0, zeroPointSix));
 
-    writeln("---------------");
+        writeln("---------------");
+    }
 
     {
         /* Direct calls to snprintf (called by format). */
@@ -168,6 +172,10 @@ void main(string[] args)
 
         snprintf(buf.ptr, buf.length, "%.*f".toStringz, 0, 1.0);
         writeln("snprintf(<buf>, <buf.length>, \"%.*f\", 0, 1.0) => ", buf.ptr.fromStringz);
+
+        double zeroPointSix = 0.6;
+        snprintf(buf.ptr, buf.length, "%.*f".toStringz, 0, zeroPointSix);
+        writeln("snprintf(<buf>, <buf.length>, \"%.*f\", 0, zeroPointSix) => ", buf.ptr.fromStringz);
 
         writeln("---------------");
 
